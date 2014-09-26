cb6e6b777dd5507496bca18f826a08e5eee76735
Alf <alf.g.jr@gmail.com>
2014-09-25 21:19:10 -0700

    cleanup and broadcast user entered fix

--- sockets.js
+++ sockets.js
@@ -1,11 +1,4 @@
-//var express = require('express');
-//var app = require('app');
-//var http = require('http').Server(app);
-//var io = require('socket.io')(http);
-
 var socketio = require('socket.io')
-
-
 
 module.exports.listen = function(app){
     io = socketio.listen(app);
@@ -23,8 +16,6 @@ module.exports.listen = function(app){
             socket.thumbnail =  profile ? profile.thumbnail : "https://avatars0.githubusercontent.com/u/1746301";
             socket.html_url = profile ? profile.html_url : "#";
 
-
-
             // Build users
             var users = [];
             var clients = io.sockets.adapter.rooms[room];
@@ -34,11 +25,10 @@ module.exports.listen = function(app){
                 obj["thumbnail"] = io.sockets.adapter.nsp.connected[id].thumbnail;
                 obj["html_url"] = io.sockets.adapter.nsp.connected[id].html_url;
                 users.push(obj);
-                console.log(io.sockets.adapter.nsp.connected[id].nickname);
             }
 
             var msg = socket.nickname + " has entered the room.";
-            io.sockets.in(room).emit('user connected', msg, users);
+            socket.broadcast.in(room).emit('user connected', msg, users);
 
             console.log('a user connected to room ' + room);
 
@@ -52,7 +42,6 @@ module.exports.listen = function(app){
                     obj["thumbnail"] = io.sockets.adapter.nsp.connected[id].thumbnail;
                     obj["html_url"] = io.sockets.adapter.nsp.connected[id].html_url;
                     users.push(obj);
-                    console.log(io.sockets.adapter.nsp.connected[id].nickname);
                 }
 
                 var msg = socket.nickname + " has left the room.";
@@ -62,7 +51,6 @@ module.exports.listen = function(app){
         });
 
         socket.on('message', function(room, msg, profile){
-//            io.emit('message', msg);
             io.sockets.in(room).emit('message', msg, profile);
             console.log('message: ' + msg);
             console.log(profile);
@@ -70,4 +58,4 @@ module.exports.listen = function(app){
 
     });
     return io
-}
+};
