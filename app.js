021845008a730c6e2ed6afbace79a89ab9af9d1a
Alf <alf.g.jr@gmail.com>
2014-09-28 20:55:04 -0700

    app restructuring

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var oauthshim = require('oauth-shim')
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var config = require('./config');
 
 var app = express();
@@ -18,17 +15,10 @@ var io = require('socket.io')(http);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // OAuth Shim
 oauthshim.init({
@@ -38,9 +28,6 @@ oauthshim.init({
 
 });
 app.all('/proxy', oauthshim.request);
-
-// Print request->response to console.
-oauthshim.debug = true;
 
 oauthshim.getCredentials = function(id,callback){
     callback(config.GITHUB_SECRET);
@@ -59,6 +46,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
+    oauthshim.debug = true;
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
